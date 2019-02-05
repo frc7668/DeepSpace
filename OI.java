a49c93edbbf30eee32f521350af9a5098d48f23c
Michael Liebis <mliebis@chapin.edu>
2019-02-05 14:28:07 -0600

    initial commit

---
+++ OI.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import frc.robot.commands.*;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * Add your docs here.
+ */
+public class OI {
+    public static final Joystick joystick_f310 = new Joystick(0);
+    public static JoystickButton button_a;
+
+     //f310 joystick mapping id
+  // axiss
+  private static final int id_left_stick_x = 0;
+  private static final int id_left_stick_y = 1;
+  private static final int id_left_trigger = 2;
+  private static final int id_right_trigger = 3;
+  private static final int id_right_stick_x = 4;
+  private static final int id_right_stick_y = 5;
+  // buttons
+  private static final int id_button_a = 1;
+  private static final int id_button_b = 2;
+  private static final int id_button_x = 3;
+  private static final int id_button_y = 4;
+  private static final int id_left_bumper = 5;
+  private static final int id_right_bumper = 6;
+  private static final int id_button_back = 7;
+  private static final int id_button_start = 8;
+  private static final int id_button_left_stick = 9;
+  private static final int id_button_right_stick = 10;
+
+  OI(){
+
+    button_a = new JoystickButton(joystick_f310, id_button_a);
+    button_a.whileHeld(new testCommand());
+
+
+  }
+
+
+
+}
---
+++ subsystems/DriveTrain.java
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SPI;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class DriveTrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+ public static ADXRS450_Gyro gyroSPI = new ADXRS450_Gyro();
+ public static AHRS ahrs= new AHRS(SPI.Port.kMXP);
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+  public void getAngle() {
+
+    System.out.println(ahrs.getAngle());
+
+  
+  }
+}
